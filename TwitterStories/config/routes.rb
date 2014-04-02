d488a3f4b616af61b2de0b0a3e6e801407508c2e
& <pair+pvergel.spragglerock@devbootcamp.com>
2014-04-02 17:12:50 -0500

    added placeholder root route

--- TwitterStories/config/routes.rb
+++ TwitterStories/config/routes.rb
@@ -1,5 +1,6 @@
 TwitterStories::Application.routes.draw do
 
+  root 'users#new' #PLACEHOLDER
   resources :users, except: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
