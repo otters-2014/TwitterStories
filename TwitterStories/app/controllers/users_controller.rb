374f808d96e7e56439217105c4ae25ffb859b3ef
& <pair+pvergel.spragglerock@devbootcamp.com>
2014-04-02 17:34:29 -0500

    User#create controller

--- TwitterStories/app/controllers/users_controller.rb
+++ TwitterStories/app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user=User.create!(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
