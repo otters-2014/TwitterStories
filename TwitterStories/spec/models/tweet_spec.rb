adedcefc547ea532267624ef81091b9ceab899dc
Kelsey & <pair+kma3a.spragglerock@devbootcamp.com>
2014-04-02 15:02:58 -0500

    added tests for tweet model

---
+++ TwitterStories/spec/models/tweet_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Tweets do
+  let(:tweet){Tweet.create(user_id: 1, text: "My dog had a really poop.  IT WAS HUGE", rating_avg: 3)}
+
+  it "should have text" do
+    expect(tweet.text).to eq "My dog had a really poop.  IT WAS HUGE"
+  end
+
+  it 'should belong to a user' do
+    expect(tweet.user.id).to eq 1
+  end
+
+  it 'should have an average rating' do
+    expect(tweet.rating_avg).to eq 3
+  end
+end
