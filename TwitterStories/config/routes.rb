abd9b50c84fbb31a6d984a3db9b8d02646c65ab2
Kelsey & <pair+kma3a.spragglerock@devbootcamp.com>
2014-04-02 21:46:21 -0500

    added tweet routes

--- TwitterStories/config/routes.rb
+++ TwitterStories/config/routes.rb
@@ -1,4 +1,7 @@
 TwitterStories::Application.routes.draw do
+
+  resources :tweets
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
