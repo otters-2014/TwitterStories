2e82922921fe10c215697c5ccd49fd1f73f2580b
Apprentice <apprentice@dbc-chicago-007.local>
2014-04-03 14:03:24 -0500

    add do/end .....

--- TwitterStories/spec/controllers/users_controller_spec.rb
+++ TwitterStories/spec/controllers/users_controller_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe UsersController do
   describe '#new' do
-    it "creates a new user"
+    it "creates a new user" do
       user = double(:user)
       User
         .should_receive(:new)
         .and_return(user)
       get :new
       expect(assigns(:user)).to eq user
+    end
   end
 end
+
+
