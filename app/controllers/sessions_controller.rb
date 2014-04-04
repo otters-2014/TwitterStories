e5a9b0a88cbb4050e84f1a90fe85e9f60ae18aac
DannyBenson <apprentice@dbc-chicago-006.local>
2014-04-04 15:36:05 -0500

    sign in and sign out implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    user = User.find(params[:id])
-    session[:user_id] = user.id
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to tweets_path(user)
+    else
+      flash[:error] = "Username or password is invalid"
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   has_many :ratings
   has_many :stories
   has_many :tweets
 
+
+  def self.authenticate(user,pass)
+    pass = Password.create(pass)
+    user.password == pass
+  end
 end
+
