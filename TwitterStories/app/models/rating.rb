9b0170abc8df26b2dfdf81a44a23f1ac5c61f5b4
Apprentice <apprentice@dbc-chicago-007.local>
2014-04-04 07:29:49 -0500

    add rating.tweet

--- TwitterStories/app/models/rating.rb
+++ TwitterStories/app/models/rating.rb
@@ -2,4 +2,8 @@ class Rating < ActiveRecord::Base
   belongs_to :rateable, polymorphic: true
   belongs_to :user
   has_many :comments
+
+  def tweet
+    self.where(rateable_type: "tweet")
+  end
 end
--- TwitterStories/spec/models/ratings_spec.rb
+++ TwitterStories/spec/models/ratings_spec.rb
@@ -4,4 +4,17 @@ describe Rating do
   it { should belong_to(:user) }
   it {should belong_to(:rateable)}
   it {should have_many(:comments)}
+
+
+  context '#tweet' do
+    it 'should return a tweet' do
+      tweet = Tweet.create(text: "poop")
+      rating = Rating.create(rateable_id: tweet.id, rateable_type: "tweet", rating: 11)
+      expect(rating.tweet).to eq tweet
+    end
+  end
+
+  context '#stories' do
+
+  end
 end
