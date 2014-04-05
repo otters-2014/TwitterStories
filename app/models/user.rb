aa1cdec1ff36bd782d02bf97a2d85c6a42abbfab
Apprentice <apprentice@dbc-chicago-007.local>
2014-04-05 13:17:28 -0500

    add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_uniqueness_of :username, message: "Oh poop. That name is already taken, I'm sorry!"
 
   has_many :ratings
   has_many :stories
