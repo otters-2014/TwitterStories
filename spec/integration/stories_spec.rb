5c14571184363da25db71da9bea4d3aa983c9c01
Kelsey & Adam Willis <pair+kma3a.whwebdev@devbootcamp.com>
2014-04-05 18:25:18 -0500

    Integration test for Story

--- spec/integration/stories_spec.rb
+++ spec/integration/stories_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
-# feature "making stories " do
-#   scenario "user can create a new story" do
-#     visit new_story_path
+feature "making stories " do
+  scenario "user can create a new story" do
+    tweet = Tweet.create(text: "something")
 
+    visit new_story_path
 
+    fill_in( 'Title', with: "a poopy day")
 
-#   end
-# end
+    find(:css, "#story_tweets_" + tweet.id.to_s + "[value='" + tweet.id.to_s  +  "']").set(true)
+
+    click_button('Create')
+
+    expect(current_path).to eq story_path(Story.last)
+
+    expect(page).to have_content("a poopy day")
+  end
+end
