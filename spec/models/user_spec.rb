54e2df5db2c3be514a91f89b2c855af9f93e14bc
Kelsey & Adam Willis <pair+kma3a.whwebdev@devbootcamp.com>
2014-04-05 23:05:15 -0500

    added test to validate unique username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe User do
   it {should have_secure_password}
+  it do
+    user = User.create(username: "asdasdasd", password: "poo", password_confirmation: "poo")
+    should validate_uniqueness_of(:username).with_message("Oh poop. That name is already taken, I'm sorry!")
+  end
 end
