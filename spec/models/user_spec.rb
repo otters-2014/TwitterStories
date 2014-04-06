2edc10af3bc0eabd17a262a11e1e5a7d97434fee
Kelsey & Adam Willis <pair+kma3a.whwebdev@devbootcamp.com>
2014-04-05 23:55:01 -0500

    added test for user model authenticate method exists

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,11 @@ describe User do
     user = User.create(username: "asdasdasd", password: "poo", password_confirmation: "poo")
     should validate_uniqueness_of(:username).with_message("Oh poop. That name is already taken, I'm sorry!")
   end
+
+  it "has an authenticate method"  do
+    User.should respond_to :authenticate
+  end
+
   it "validates a users password" do
     user = User.create(username: "qweqwe", password: "poo", password_confirmation: "poo")
     user.authenticate("poo")
